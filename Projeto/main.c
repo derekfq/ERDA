22520ab924db84d6b2e8790c69d7a66338afe2d8
marcoadenadai <32654919+marcoadenadai@users.noreply.github.com>
2020-05-24 19:13:07 -0300

    etapa 3

--- Projeto/main.c
+++ Projeto/main.c
@@ -12,7 +12,22 @@ int main(int argc, char ** argv){
     CadastraPonto(&ListaDePontos,oeste,baixa);
     CadastraPonto(&ListaDePontos,centro,alta);
     //NÃO CADASTREM MAIS DE 5 PONTOS!!!
+    //imprime_Pontos(ListaDePontos);
+
+    Itens * I = inicializa_Itens();
+    item itm = set_item("Cachorro Quente", 2, 15.00, "Sem picles, por favor! :-) Obrigado.");
+    push_item(I,itm);
+    itm = set_item("Xturbo", 1, 30.00, NULL);
+    push_item(I,itm);
+
+    CadastraPedido(ListaDePontos, I);
+    //Valor Total Vendido do Ponto so sera escrito no momento de atendimento dos pedidos
     imprime_Pontos(ListaDePontos);
+
+
+
+
+
     libera_Pontos(ListaDePontos);
     return 0;
 
