7b5e5fbb605b7f56fdedbe318adb85c36372b911
marcoadenadai <32654919+marcoadenadai@users.noreply.github.com>
2020-05-31 15:35:11 -0300

    Add files via upload

--- Projeto/main.c
+++ Projeto/main.c
@@ -4,30 +4,18 @@
 
 int main(int argc, char ** argv){
     Pontos * ListaDePontos = NULL;
-    /*OBS: Vão existir no maximo 5 pontos, um para cada regiao.
-    Senão da conflituo, isso foi conversado com a patricia.*/
+    /*
     CadastraPonto(&ListaDePontos,norte,baixa);
     CadastraPonto(&ListaDePontos,sul,media);
     CadastraPonto(&ListaDePontos,leste,alta);
     CadastraPonto(&ListaDePontos,oeste,baixa);
-    CadastraPonto(&ListaDePontos,centro,alta);
-    //NÃO CADASTREM MAIS DE 5 PONTOS!!!
-    //imprime_Pontos(ListaDePontos);
+    CadastraPonto(&ListaDePontos,centro,alta);*/
 
-    Itens * I = inicializa_Itens();
-    item itm = set_item("Cachorro Quente", 2, 15.00, "Sem picles, por favor! :-) Obrigado.");
-    push_item(I,itm);
-    itm = set_item("Xturbo", 1, 30.00, NULL);
-    push_item(I,itm);
-
-    CadastraPedido(ListaDePontos, I);
-    //Valor Total Vendido do Ponto so sera escrito no momento de atendimento dos pedidos
+    gera_Info(&ListaDePontos);
+    printf("\n\n\n\n");
     imprime_Pontos(ListaDePontos);
 
-
-
-
-
+    printf("\n\n\n\n\n\n\n\n\n");
     libera_Pontos(ListaDePontos);
     return 0;
 
