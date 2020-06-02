335ba0dd9328751f9b3abdc99d74d62de6262558
Lucas Cicconi <55239117+LucasCicconi@users.noreply.github.com>
2020-06-02 19:03:23 -0300

    Add files via upload

---
+++ items.h
@@ -0,0 +1,110 @@
+#ifndef ITEMS_H_INCLUDED
+#define ITEMS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct item {
+    char nome[64];          //item: descricao do produto
+    unsigned short qntd;    //quantidade
+    float v_unidade;        //valor da unidade em R$
+    float v_total;          //valor total do item do pedido em R$
+    char obs[256];          //campo nao obrigatorio
+    struct item* prox;     //ponteiro para proxima pilha
+} item;
+
+typedef struct itens {
+    item* topo;
+} Itens; //contem vetor de item
+
+// Funcao que inicializa 
+Itens* inicializa_Itens() {
+    Itens* i;
+    i = (Itens*)malloc(sizeof(Itens));
+    i->topo = NULL;
+    return i;
+}
+
+item set_item(const char nome[64], unsigned short qntd, float valor_unidade, const char obs[256]) {
+    item ret;
+    strcpy(ret.nome, nome);
+    ret.qntd = qntd;
+    ret.v_unidade = valor_unidade;
+    ret.v_total = valor_unidade * qntd;
+    if (obs)
+        strcpy(ret.obs, obs);
+    else
+        memset(ret.obs, '\0', 256);
+    ret.prox = NULL;
+    return ret;
+}
+
+void push_item(Itens* I, item info) {
+    item* aux = (item*)malloc(sizeof(item));
+    strcpy(aux->nome, info.nome);
+    aux->qntd = info.qntd;
+    aux->v_unidade = info.v_unidade;
+    aux->v_total = info.v_total;
+    strcpy(aux->obs, info.obs);
+    aux->prox = NULL;
+    aux->prox = I->topo;
+    I->topo = aux;
+}
+
+item pop_item(Itens* I) {
+    if (!I) {
+        printf("Pilha de Itens VAZIA.\n");
+        exit(1);
+    }
+    item ret;
+    strcpy(ret.nome, I->topo->nome);
+    ret.qntd = I->topo->qntd;
+    ret.v_unidade = I->topo->v_unidade;
+    ret.v_total = I->topo->v_total;
+    strcpy(ret.obs, I->topo->obs);
+    ret.prox = NULL;
+    item* tmp = I->topo->prox;
+    free(I->topo);
+    I->topo = tmp;
+    return ret;
+}
+
+int vazia_Itens(Itens* I) {
+    if (!I->topo)
+        return 1; //pilha vazia
+    return 0;
+}
+
+void libera_Itens(Itens* I) {
+    item* i = I->topo, * tmp;
+    while (i) {
+        tmp = i->prox;
+        free(i);
+        i = tmp;
+    }
+    free(I);
+    return;
+}
+
+void imprime_Itens(Itens* I) {
+    if (!I->topo)
+        printf("\n[Pilha de Itens]: VAZIA!\n");
+    else {
+        printf("\n[Pilha de Itens]: <TOPO> **********\n");
+        item* x;
+        for (x = I->topo; x; x = x->prox) {
+            printf("Nome : %s\n", x->nome);
+            printf("Qtd. : x%d\n", x->qntd);
+            printf("Und. : R$ %.2f\n", x->v_unidade);
+            if (x->obs[0] != '\0')
+                printf("OBS* : %s\n", x->obs);
+            printf("Total: R$ %.2f\n", x->v_total);
+
+
+            printf("***********************************\n");
+        }
+    }
+}
+
+#endif
---
+++ pedidos.h
@@ -0,0 +1,151 @@
+#ifndef PEDIDOS_H_INCLUDED
+#define PEDIDOS_H_INCLUDED
+#include "items.h"
+/*ESTA BIBLIOTECA SOMENTE DEFINE A ESTRUTURA DOS PEDIDOS E IMPLEMENTA SEU FUNCIONAMENTO EM FILA
+  EH IMPORTANTE LEMBRAR QUE ELA NÃO IMPLEMENTA O FUNCIONAMENTO REAL DOS PEDIDOS NO SISTEMA!.*/
+
+//typedef enum Reg { centro = 1, sul = 2, leste = 3, norte = 4, oeste = 5 } Regiao;
+
+typedef struct pedido {
+    unsigned int num; //id do pedido
+    int regiao; //regiao do pedido
+    unsigned int tempo; //tempo de entrega
+    float v_pedido; //valor total do pedido, calculado automaticamente pelo sistema
+    Itens* I;    // PILHA de ITENS DO PEDIDO!
+    struct pedido* prox;
+} pedido;
+
+typedef struct pedidos {
+    pedido* ini;
+    pedido* fim;
+} Pedidos; //ATENCAO, OS PEDIDOS SERAO UMA FILA!!!
+
+int vazia_Pedidos(Pedidos* f) {
+    if (f->ini == NULL) return 1;
+    return 0;
+}
+
+Pedidos* inicializa_Pedidos() {
+    Pedidos* f = (Pedidos*)malloc(sizeof(Pedidos));
+    f->ini = f->fim = NULL;
+    return f;
+}
+
+void insere_Pedidos(Pedidos* P, pedido info) { //Insere novo pedido "info" no fim da fila
+    pedido* novo = (pedido*)malloc(sizeof(pedido));
+    novo->num = info.num;
+    novo->regiao = info.regiao;
+    novo->tempo = info.tempo;
+    novo->v_pedido = info.v_pedido;
+    novo->I = info.I;
+    novo->prox = NULL;
+    if (P->fim)//se o fim nao for vazio, insere no fim
+        P->fim->prox = novo;
+
+    P->fim = novo;
+    if (!P->ini)//se a fila estava vazia antes
+        P->ini = P->fim;
+}
+
+pedido retira_Pedidos(Pedidos* P) {//Retira do inicio da fila de Pedidos, "atende"
+    pedido ret;
+    if (vazia_Pedidos(P)) {
+        printf("Fila de Pedidos vazia, nao eh possivel retirar.\n");
+        exit(0);
+    }
+    ret.num = P->ini->num;
+    ret.regiao = P->ini->regiao;
+    ret.tempo = P->ini->tempo;
+    ret.v_pedido = P->ini->v_pedido;
+    ret.I = P->ini->I;
+    ret.prox = P->ini->prox;// manter??? ou usar =  P->ini->prox;
+    pedido* rmv = P->ini;
+    //if (P->ini->prox)
+    P->ini = P->ini->prox;
+    free(rmv);
+    if (!P->ini) //fica ficou vazia
+        P->fim = NULL;
+    return ret;
+}
+
+int GetTempoEspera(Pedidos* P, int id);
+
+void imprime_Pedidos(Pedidos* P) {
+    pedido* aux;
+    printf("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
+    for (aux = P->ini; aux; aux = aux->prox) {
+        printf("Pedido #%d\n", aux->num);
+        printf("Regiao: ");
+        switch (aux->regiao) {
+        case 1:
+            printf("Centro\n");
+            break;
+        case 2:
+            printf("Zona Sul\n");
+            break;
+        case 3:
+            printf("Zona Leste\n");
+            break;
+        case 4:
+            printf("Zona Norte\n");
+            break;
+        case 5:
+            printf("Zona Oeste\n");
+            break;
+        default:
+            printf("?????\n");
+            break;
+        }
+        printf("Tempo de Entrega: %d\n", GetTempoEspera(P, aux->num));
+        printf("Valor do Pedido: R$%.2f\n", aux->v_pedido);
+
+        //ta certo ainda
+        imprime_Itens(aux->I);
+        printf("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
+    }
+}
+
+void libera_Pedidos(Pedidos* P) {
+    pedido* rmv, * aux = P->ini;
+    while (aux) {
+        rmv = aux;
+        libera_Itens(aux->I);
+        aux = aux->prox;
+        free(rmv);
+    }
+    free(P);
+}
+
+int GetTempoEspera(Pedidos* P, int id) {
+    pedido* aux;
+    int ret = 0;
+    for (aux = P->ini; aux && aux->num <= id; aux = aux->prox) {
+        ret += aux->tempo;
+    }
+    return ret + 15;
+}
+
+
+void excluir_Pedido(Pedidos* Pedidos_Carrinho, int id){
+
+    Pedidos* auxPedidos = inicializa_Pedidos();
+    pedido  auxInfo;
+
+    while (!vazia_Pedidos(Pedidos_Carrinho))
+    {
+        auxInfo = retira_Pedidos(Pedidos_Carrinho);
+        if (auxInfo.num != id)
+        {
+            insere_Pedidos(auxPedidos, auxInfo);
+        }
+    }
+
+    while (!vazia_Pedidos(auxPedidos))
+    {
+        auxInfo = retira_Pedidos(auxPedidos);
+        insere_Pedidos(Pedidos_Carrinho, auxInfo);
+    }
+
+}
+
+#endif
