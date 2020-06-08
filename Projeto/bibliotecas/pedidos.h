3053fbdde737d559afb9d1f2278cc3ae5fbc9fb8
Lucas Cicconi <55239117+LucasCicconi@users.noreply.github.com>
2020-06-07 21:20:39 -0300

    pedidos comentado

--- Projeto/bibliotecas/pedidos.h
+++ Projeto/bibliotecas/pedidos.h
@@ -4,52 +4,56 @@
 /*ESTA BIBLIOTECA SOMENTE DEFINE A ESTRUTURA DOS PEDIDOS E IMPLEMENTA SEU FUNCIONAMENTO EM FILA
   EH IMPORTANTE LEMBRAR QUE ELA NÃO IMPLEMENTA O FUNCIONAMENTO REAL DOS PEDIDOS NO SISTEMA!.*/
 
-typedef enum Reg {centro=1, sul=2, leste=3, norte=4, oeste=5} Regiao;
+typedef enum Reg { centro = 1, sul = 2, leste = 3, norte = 4, oeste = 5 } Regiao;
 
-typedef struct pedido{
-    unsigned int num; //id do pedido
-    Regiao regiao; //regiao do pedido
-    unsigned int tempo; //tempo de entrega
-    float v_pedido; //valor total do pedido, calculado automaticamente pelo sistema
-    Itens * I;    // PILHA de ITENS DO PEDIDO!
-    struct pedido * prox;
+typedef struct pedido {
+    unsigned int num;              //id do pedido
+    Regiao regiao;                 //regiao do pedido
+    unsigned int tempo;            //tempo de entrega
+    float v_pedido;                //valor total do pedido, calculado automaticamente pelo sistema
+    Itens* I;                      // PILHA de ITENS DO PEDIDO!
+    struct pedido* prox;
 } pedido;
 
-typedef struct pedidos{
-    pedido * ini;
-    pedido * fim;
+typedef struct pedidos {
+    pedido* ini;
+    pedido* fim;
 } Pedidos; //ATENCAO, OS PEDIDOS SERAO UMA FILA!!!
 
-int vazia_Pedidos (Pedidos* f){
-    if (f->ini==NULL) return 1;
+//  verifica se a fila de pedidos está vazia
+int vazia_Pedidos(Pedidos* f) {
+    if (f->ini == NULL) return 1;
     return 0;
 }
 
-Pedidos* inicializa_Pedidos (){
-    Pedidos* f = (Pedidos*) malloc(sizeof(Pedidos));
+//  inicializa a pilha
+Pedidos* inicializa_Pedidos() {
+    Pedidos* f = (Pedidos*)malloc(sizeof(Pedidos));    //alocando espaço da struct
     f->ini = f->fim = NULL;
     return f;
 }
 
-void insere_Pedidos(Pedidos * P, pedido info){ //Insere novo pedido "info" no fim da fila
-    pedido * novo = (pedido *) malloc(sizeof(pedido));
+//  insere novo pedido "info" no fim da fila
+void insere_Pedidos(Pedidos* P, pedido info) {
+    pedido* novo = (pedido*)malloc(sizeof(pedido));
     novo->num = info.num;
     novo->regiao = info.regiao;
     novo->tempo = info.tempo;
     novo->v_pedido = info.v_pedido;
     novo->I = info.I;
     novo->prox = NULL;
-    if(P->fim)//se o fim nao for vazio, insere no fim
+    if (P->fim)//se o fim nao for vazio, insere no fim
         P->fim->prox = novo;
 
-        P->fim = novo;
-    if(!P->ini)//se a fila estava vazia antes
+    P->fim = novo;
+    if (!P->ini)//se a fila estava vazia antes
         P->ini = P->fim;
 }
 
-pedido retira_Pedidos(Pedidos * P){//Retira do inicio da fila de Pedidos, "atende"
+//  retira do inicio da fila de Pedidos, "atende"
+pedido retira_Pedidos(Pedidos* P) {
     pedido ret;
-    if(vazia_Pedidos(P)){
+    if (vazia_Pedidos(P)) {
         printf("Fila de Pedidos vazia, nao eh possivel retirar.\n");
         exit(0);
     }
@@ -58,53 +62,57 @@ pedido retira_Pedidos(Pedidos * P){//Retira do inicio da fila de Pedidos, "atend
     ret.tempo = P->ini->tempo;
     ret.v_pedido = P->ini->v_pedido;
     ret.I = P->ini->I;
-    ret.prox = NULL;// manter??? ou usar =  P->ini->prox;
-    pedido * rmv = P->ini;
+    ret.prox = NULL;
+
+    pedido* rmv = P->ini;
     P->ini = P->ini->prox;
     free(rmv);
-    if(!P->ini) //fica ficou vazia
+
+    if (!P->ini) //fila ficou vazia
         P->fim = NULL;
     return ret;
 }
 
-int GetTempoEspera(Pedidos * P, int id);
+int GetTempoEspera(Pedidos* P, int id); //Declaração pra utilização no Imprime_Pedidos
 
-void imprime_Pedidos(Pedidos * P){
-    pedido * aux;
+//  percorre a fila, imprimindo todos os valores de todos os Itens e seu tempo total de entrega
+void imprime_Pedidos(Pedidos* P) {
+    pedido* aux;
     printf("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
-    for(aux=P->ini;aux;aux=aux->prox){
-        printf("Pedido #%d\n",aux->num);
+    for (aux = P->ini; aux; aux = aux->prox) {
+        printf("Pedido #%d\n", aux->num);
         printf("Regiao: ");
-        switch(aux->regiao){
-            case centro:
-                printf("Centro\n");
-                break;
-            case sul:
-                printf("Zona Sul\n");
-                break;
-            case leste:
-                printf("Zona Leste\n");
-                break;
-            case norte:
-                printf("Zona Norte\n");
-                break;
-            case oeste:
-                printf("Zona Oeste\n");
-                break;
-            default:
-                printf("?????\n");
-                break;
+        switch (aux->regiao) {
+        case centro:
+            printf("Centro\n");
+            break;
+        case sul:
+            printf("Zona Sul\n");
+            break;
+        case leste:
+            printf("Zona Leste\n");
+            break;
+        case norte:
+            printf("Zona Norte\n");
+            break;
+        case oeste:
+            printf("Zona Oeste\n");
+            break;
+        default:
+            printf("?????\n");
+            break;
         }
-        printf("Tempo de Entrega: %d\n",GetTempoEspera(P,aux->num));
-        printf("Valor do Pedido: R$%.2f\n",aux->v_pedido);
+        printf("Tempo de Entrega: %d\n", GetTempoEspera(P, aux->num));    
+        printf("Valor do Pedido: R$%.2f\n", aux->v_pedido);
         imprime_Itens(aux->I);
         printf("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
     }
 }
 
-void libera_Pedidos(Pedidos * P){
-    pedido * rmv, * aux = P->ini;
-    while(aux){
+//  percorre a fila, e libera memoria (free)
+void libera_Pedidos(Pedidos* P) {
+    pedido* rmv, * aux = P->ini;
+    while (aux) {
         rmv = aux;
         libera_Itens(aux->I);
         aux = aux->prox;
@@ -113,17 +121,15 @@ void libera_Pedidos(Pedidos * P){
     free(P);
 }
 
-int GetTempoEspera(Pedidos * P, int id){
-	pedido * aux;
-	int ret = 0;
-	for(aux=P->ini;aux && aux->num <= id;aux=aux->prox){
-		ret+=aux->tempo;
-	}
-	return ret + 15;
+//  Percorre a fila, encontra todos os tempos de cada pedido, e os soma
+int GetTempoEspera(Pedidos* P, int id) {
+    pedido* aux;
+    int ret = 0;
+    //enquanto aux não for NULL, e nem id for igual ao id do pedido,para somar todos atraz do pedido alvo
+    for (aux = P->ini; aux && aux->num <= id; aux = aux->prox) {
+        ret += aux->tempo;
+    }
+    return ret + 15; //+15 do frete
 }
-
-
-
-
 
 #endif
